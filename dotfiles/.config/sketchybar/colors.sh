dc61fbe77e2c89f9dae933b0b83d40b241d09522
Joey <fangjin98@mail.ustc.edu.cn>
2023-05-15 20:03:19 +0800

    update sketchybar

--- dotfiles/.config/sketchybar/colors.sh
+++ dotfiles/.config/sketchybar/colors.sh
@@ -14,10 +14,10 @@ export TRANSPARENT=0x00000000
 
 # General bar colors
 export BAR_COLOR=$TRANSPARENT
-export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
-export ICON_COLOR=$WHITE # Color of all icons
-export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0x603c3e4f
+export BAR_BORDER_COLOR=0xff494d64
+export ICON_COLOR=$WHITE
+export LABEL_COLOR=$WHITE
+export BACKGROUND_1=0x66494d64
 export BACKGROUND_2=0x60494d64
 
 export POPUP_BACKGROUND_COLOR=0xff1e1e2e
--- dotfiles/.config/sketchybar/items/spaces.sh
+++ dotfiles/.config/sketchybar/items/spaces.sh
@@ -10,21 +10,21 @@ do
 
   space=(
     associated_space=$sid
+    padding_right=2
+    padding_left=2
     icon="${SPACE_ICONS[i]}"
     icon.padding_left=10
     icon.padding_right=10
-    padding_left=2
-    padding_right=2
-    label.padding_right=20
     icon.highlight_color=$RED
+    # label.padding_right=20
     label.color=$GREY
     label.highlight_color=$WHITE
-    label.font="sketchybar-app-font:Regular:16.0"
+    label.font="$FONT:Regular:16.0"
     label.y_offset=-1
+    label.drawing=off
     background.color=$BACKGROUND_1
     background.border_color=$BACKGROUND_2
     background.drawing=off
-    label.drawing=off
     script="$PLUGIN_DIR/space.sh"
   )
 
